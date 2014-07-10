7f15c6dd9522273663e902baf3a119d5c968ef70
Dave Boling <kadowki@gmail.com>
2014-07-09 23:31:55 -0500

    Completed the program!

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var tankSize = prompt('What is your gas tank\'s size? ');
 tankSize = parseInt(tankSize);
 
 var vehicleType = prompt('What is your vehicle type? (car or truck) ');
-vehicleType = parseInt(vehicleType);
 
 var speed = prompt('What is your speed? (MPH) ');
 speed = parseInt(speed);
@@ -25,25 +24,28 @@ var trueMpg = 0;
 fullTankCost = tankSize * gasPrice;
 
 //What is the true MPG of the vehicle?
-if(speed <= 55){ trueMpg = 25; }
-else if (vehicleType === 'truck'){ trueMpg = mpg - ((speed - speedThreshold) * 3); }
-else if (vehicleType === 'car'  ){ trueMpg = mpg - (speed - speedThreshold); }
-console.log( trueMpg );
+//Are we less than our offset?
+if (speed > speedThreshold && vehicleType === 'truck'){ trueMpg = mpg - ((speed - speedThreshold) * 3); console.log(trueMpg); }
+else if (speed > speedThreshold && vehicleType === 'car'){ trueMpg = mpg - (speed - speedThreshold); console.log(trueMpg);}
+else { trueMpg = mpg; }
 
-//Check to see if we've gone below 10 MPG
-//If we did go over, set it equal to the offset since this is the limit
-if (trueMpg < speedOffset){
-	trueMpg = speedOffset;
-}
+//Check to see how low our trueMpg is. If it's too low, we have to set it to the speedOffset.
+console.log(trueMpg);
+if (trueMpg <= speedOffset){ trueMpg = speedOffset;}
 
 //Number of miles per tank of gas
 var milesPerTank = trueMpg * tankSize;
 
+//Total Trip Cost
+var totalTripCost = (Math.round((milesToGo/trueMpg*100)/100) * gasPrice);
+var totalFillUps = (Math.round((totalTripCost/fullTankCost) * 10 ) / 10);
+
+//Output all calculations
 console.log("True MPG " + trueMpg);
 console.log("Full tank cost " + fullTankCost);
 console.log("Miles per tank " + milesPerTank);
-console.log('Cost of gas for a ' + milesToGo + ' mile trip is $' + ((milesToGo/trueMpg) * gasPrice));
-console.log('You will have to fill up your tank ');
+console.log('Cost of gas for a ' + milesToGo + ' mile trip is $' + totalTripCost);
+console.log('You will have to fill up your tank ' + totalFillUps + ' times.' );
 
 
 
